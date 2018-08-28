db23a328f2faafe048411590dd480128d560aa84
Michael Roberts <michaelroberts@Michaels-MacBook-Air.local>
2018-08-27 18:38:12 -0700

    adds models and custom remote method.

---
+++ common/models/card.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(Card) {
+
+};
+
+// {
+//   "question": "What is the difference between == and === ?",
+//   "points": 1,
+//   "difficulty": 1,
+//   "category": "JavaScript",
+//   "answers": [
+//     {
+//       "title": "== assigns while === compares",
+//       "isCorrect": false
+//     },
+//     {
+//       "title": "== checks for loose equality while === checks for strict equality",
+//       "isCorrect": true
+//     },
+//     {
+//       "title": "=== assigns while == compares",
+//       "isCorrect": false
+//     },
+//     {
+//       "title": "=== checks for loose equality while == checks for strict equality",
+//       "isCorrect": false
+//     }
+//   ]
+// }
+
---
+++ common/models/game.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(Game) {
+
+  Game.getPlayers = function(id, cb) {
+    const { Player, Result } = Game.app.models;
+    Result.find({where: {gameId: id}})
+      .then((results) => {
+        return Promise.all(results.map((result) => {
+          return Player.findById(result.playerId)
+            .then(player => player)
+            .catch(error => console.log(error));
+        }));
+      })
+      .then((allResults) => cb(null, allResults.filter(x => !!x)))
+      .catch(err => console.log(err));
+  };
+
+  Game.remoteMethod('getPlayers', {
+    description: 'Returns all players from a game.',
+    accepts: {arg: 'id', type: 'number'},
+    http: {path: '/getPlayers', verb: 'get'},
+    returns: {arg: 'data', type: 'object', root: true},
+  });
+};
